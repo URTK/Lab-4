4ab165f2ea331ef8fd47f1b28dcc2597483a9757
УРТК <43788393+URTK@users.noreply.github.com>
2018-10-08 09:40:54 +0500

    V1.0

--- Main.py
+++ Main.py
@@ -1,2 +1,17 @@
-full_names = [u"Иванов Иван Иванович", u"Петров Петр Петрович"]
-short_names = [' '.join([fn.split(' ')[0],fn.split(' ')[0][1]]) for fn in full_names]
+FullName = str(input('Введите ФИО '))
+
+    # Основное задание
+
+print('К-во символов в троке : ', len(FullName))
+
+StringA = FullName.count('А')
+Stringa = FullName.count('а')
+print ('К-во "A" в ФИО', StringA + Stringa)
+
+    # Разделение ФИО на Ф И О
+
+Nazar = (FullName.split())
+
+    # Задание по варианту
+
+print('В фамилии ', Nazar[1],' содержится', Nazar[1].count('а'),' букв "А"')
